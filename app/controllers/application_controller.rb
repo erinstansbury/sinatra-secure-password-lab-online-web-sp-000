bda8834be1bdd2c84f7da2def22f52f550c22da0
Erin Stansbury <ekstansbury@gmail.com>
2021-01-03 01:56:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,9 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
     if user.save
-      
+      redirect "/login"
+    else
+      redirect "/"
 
   end
 
