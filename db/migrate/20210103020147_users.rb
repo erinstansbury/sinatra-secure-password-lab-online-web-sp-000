66af300a0015a2dae423c375e3851a16d2a3bdcc
Erin Stansbury <ekstansbury@gmail.com>
2021-01-03 02:03:07 +0000

    Automatically backed up by Learn

--- db/migrate/20210103020147_users.rb
+++ db/migrate/20210103020147_users.rb
@@ -2,7 +2,8 @@ class Users < ActiveRecord::Migration[5.1]
   def up 
     create_table :users do |t|
       t.string :username
-      t.password :password
+      t.password :password_digest
+      
     
   end
 end
